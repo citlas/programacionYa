c9f9ed482415882fda05e6699dddb8d2522b2f59
Citlalli Prado <citlalli.prado@gmail.com>
2019-07-01 19:57:17 +0200

    Created project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,125 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'programacionYa';
+  nombre = 'Rodriguez Pablo';
+  edad = 40;
+  email = 'rpablo@gmail.com';
+  sueldos = [1700, 1600, 1900];
+  activo = true;
+  //ejercicio 9
+  valor1:number;
+  valor2:number;
+  valor3:number;
+  resultado:string;
+
+  esActivo(){
+    if(this.activo){
+      return 'Trabajador Activo';
+     } else {
+      return 'Trabajador Inctivo';
+    }
+  }
+
+  ultimos3Sueldos(){
+    let suma=0;
+    for(let x=0; x<this.sueldos.length; x++){
+      suma+=this.sueldos[x];
+      return suma;
+    }
+  }
+
+  //ejercicio 6
+  contador=1;
+  incrementar(){
+    this.contador++;
+  }
+  decrementar(){
+    this.contador--;
+  }
+
+  //ejercicio 7
+  nombre2="";
+  apellido2="";
+
+  art={
+    codigo:null,
+    descripcion:null,
+    precio:null
+  }
+
+  articulos =[{codigo:1, descripcion:'papas',precio:10.55},
+    {codigo:2, descripcion:'manzanas',precio:12.10},
+    {codigo:3, descripcion:'melón',precio:52.30},
+    {codigo:4, descripcion:'cebollas',precio:17},
+    {codigo:5, descripcion:'calabaza',precio:20},
+  ];
+
+  hayRegistros(){
+    return this.articulos.length>0
+  }
+
+  agregar(){
+    for (let x = 0; x < this.articulos.length; x++) 
+      if(this.articulos[x].codigo==this.art.codigo){
+        alert('ya existe un articulo con dicho codigo')
+        return;
+      } 
+        this.articulos.push({codigo: this.art.codigo,
+        descripcion: this.art.descripcion,
+      precio: this.art.precio})
+      this.art.codigo=null;
+      this.art.descripcion=null;
+      this.art.precio=null;
+      }
+    
+  
+
+  borrar(art){
+    for (let x = 0; x < this.articulos.length; x++) {
+      if(this.articulos[x].codigo==art.codigo){
+        this.articulos.splice(x,1);
+        return;
+      } 
+    }
+  }
+
+  seleccionar(art){
+    this.art.codigo=art.codigo;
+    this.art.descripcion=art.descripcion;
+    this.art.precio=art.precio;
+  }
+
+  modificar(){
+    for (let x = 0; x < this.articulos.length; x++) 
+      if(this.articulos[x].codigo==this.art.codigo){
+        this.articulos[x].descripcion=this.art.descripcion;
+        this.articulos[x].precio=this.art.precio;
+        return;
+      } 
+      alert('no existe el código de articulo ingresado')
+    
+  }
+
+  //ejercicio 9
+  constructor(){
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+  }
+
+  retornarAleatorio(){
+    return Math.trunc(Math.random() * 6) + 1;
+  }
+
+  tirar(){
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+    if(this.valor1 ==this.valor2 && this.valor1 ==this.valor3){
+      this.resultado ='Ganó';
+    } else {
+      this.resultado ='Perdió';
+    }
+  }
+  
 }
